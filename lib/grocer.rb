50d79f403749f54c048c3bf773613f50fdee3549
Phong Lo <phonglo724@gmail.com>
2021-01-21 23:08:22 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -11,13 +11,10 @@ def find_item_by_name_in_collection(name, collection)
 end #ends method
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   new_array = []
   index = 0
   while index < cart.length 
+    new_cart_item = find_item_by_name_in_collection(cart[index][:item])
     if cart[index][:item][:price][:clearance] == cart
    
     end # ends if statement
